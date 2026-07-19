191b7139cbd65386d1384462c2ebbe8522d23b98
Priyanshi Yadav <yadavpriyanshij@gmail.com>
2026-07-19 20:41:43 +0530

    Time: 1 ms (59.3%), Space: 43.3 MB (45.44%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,9 +1,18 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
         //Concatenate
-        if (s.length() != goal.length())
-    return false;
+        StringBuilder sb = new StringBuilder(s);
 
-return (s + s).contains(goal);
+for (int i = 0; i < s.length(); i++) {
+
+    if (sb.toString().equals(goal))
+        return true;
+
+    char first = sb.charAt(0);
+    sb.deleteCharAt(0);
+    sb.append(first);
+}
+
+return false;
     }
 }
