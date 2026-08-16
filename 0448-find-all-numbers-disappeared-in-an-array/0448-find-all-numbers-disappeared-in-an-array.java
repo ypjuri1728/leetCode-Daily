275e24c1837ff71ff9f31ec83eef8c98b036fe33
Priyanshi Yadav <yadavpriyanshij@gmail.com>
2026-08-16 21:52:14 +0530

    Time: 9 ms (37.19%), Space: 66.9 MB (92.15%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,16 +1,37 @@
+// class Solution {
+//     public List<Integer> findDisappearedNumbers(int[] nums) {
+//         int n = nums.length;
+//         List<Integer> ans = new ArrayList<>(); // for output 
+//         HashSet<Integer> set = new HashSet<>();// check existing number in range 
+//         for(int i =0;i<n;i++){
+//             set.add(nums[i]);
+//         }
+//         for(int j = 1;j<= n;j++){
+//             if(!set.contains(j)){
+//                 ans.add(j);
+//             }
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         int n = nums.length;
-        List<Integer> ans = new ArrayList<>(); // for output 
-        HashSet<Integer> set = new HashSet<>();// check existing number in range 
-        for(int i =0;i<n;i++){
-            set.add(nums[i]);
+        List<Integer> ans = new ArrayList<>();
+
+        // Mark numbers as negative
+        for (int i = 0; i < n; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            nums[index] = -Math.abs(nums[index]);
         }
-        for(int j = 1;j<= n;j++){
-            if(!set.contains(j)){
-                ans.add(j);
+
+        // Find missing numbers
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                ans.add(i + 1);
             }
         }
         return ans;
     }
 }
+// current number --> Math.abs(nums[i]) --> number - 1 --> index --> make nums[index] negative
