d25f9ba083f77dd653ac213b0eba117ae8988727
Priyanshi Yadav <yadavpriyanshij@gmail.com>
2026-08-23 23:24:45 +0530

    Time: 28 ms (51.23%), Space: 45.7 MB (60.27%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -3,16 +3,20 @@ class Solution {
         Arrays.sort(nums);
         int count =0;
         int n = nums.length;
+        // Fix the largest side from right to left
         for(int i =n-1;i >=2;i--) {//last to top run
         int left =0;
         int right = i-1;
         while(left <right){
+               // Triangle condition: a + b > c
             if(nums[left] + nums[right] >nums[i]){
+                 // All values between left and right
+            // can form a triangle with nums[right] and nums[i]
                 count += right-left;
-                right--;
+                right--;// Try a smaller right side
             }
             else{
-                left++;
+                left++;// Sum is too small, increase left
             }
         }
     }
