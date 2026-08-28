300db8af499ee73e97c8df6585f03d94967e0bdb
Priyanshi Yadav <yadavpriyanshij@gmail.com>
2026-08-28 23:49:41 +0530

    Time: 1 ms (100%), Space: 48.9 MB (32.42%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,36 +1,46 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        ArrayList<Integer> list = new ArrayList<>();
+        if(nums1.length>nums2.length) {
+return findMedianSortedArrays(nums2, nums1);// call function again for min length array 
+        }
+        int n = nums1.length;//left array size
+        int m = nums2.length;//rigth array size
 
-        int i = 0;
-        int j = 0;
-        //Compare both
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] < nums2[j]) {
-                list.add(nums1[i]);
-                i++;
-            } else {
-                list.add(nums2[j]);
-                j++;
-            }
+        int low=0;//left is small alwyas
+        int high =n; //nums1 is the smaller array.
+
+        while(low<=high){
+            int px= low+(high-low)/2;
+            int py =  (m+n+1)/2-px;  // to make eaiser
+
+            //left partition
+            int x1= (px ==0)? Integer.MIN_VALUE:nums1[px-1];
+            int x2 = (py ==0)? Integer.MIN_VALUE:nums2[py-1];
+
+            //right partiton
+            int x3 =(px ==n)? Integer.MAX_VALUE:nums1[px];
+            int x4 =(py ==m)? Integer.MAX_VALUE:nums2[py];
+
+
+
+    if(x1<= x4 && x2<= x3){
+        //odd
+        if((m+n) % 2 ==1){
+            return Math.max(x1,x2);
         }
-        //  nums1 may have leftovers
-        while (i < nums1.length) {
-            list.add(nums1[i]);
-            i++;
+        //even
+        else{
+            return (Math.max(x1,x2)+Math.min(x3,x4))/2.0;
         }
-        //  nums2 may have leftovers
-        while (j < nums2.length) {
-            list.add(nums2[j]);
-            j++;
+    }
+    if(x1>x4){
+        high = px-1;
+    }
+    else{
+        low= px+1;
+    }
         }
 
-        int n = list.size();
-        if (n % 2 == 0) {
-            // EVEN → two middle elements
-            return (list.get(n / 2 - 1) + list.get(n / 2)) / 2.0;
-        }
-        // ODD → one middle element
-        return list.get(n / 2);
+        return -1;
     }
 }
