3285286ba9541d8ce62839d54d092735d1f504a3
Priyanshi Yadav <yadavpriyanshij@gmail.com>
2026-09-08 11:51:52 +0530

    Time: 0 ms (100%), Space: 48.1 MB (71.6%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,42 +1,39 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int n = nums.length;
-        int first = -1;
+        int first =-1;
         int last = -1;
-
-        // Find first occurrence
-        int left = 0;
-        int right = n - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
+        int n = nums.length;
+        int left =0;
+        int right = n-1;
+        //first ele
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(nums[mid] == target){
                 first = mid;
-                right = mid - 1;
+                right = mid-1;
             }
-            else if (nums[mid] < target) {
-                left = mid + 1;
+            else if(nums[mid] <target){
+                left = mid+1;
             }
-            else {
-                right = mid - 1;
+            else{
+                right = mid-1;
             }
         }
-
-        // Find last occurrence
-        left = 0;
-        right = n - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
+        left =0;
+        right = n-1;
+        while(left<= right){
+            int mid = left+(right-left)/2;
+            if(nums[mid] == target){
                 last = mid;
-                left = mid + 1;
+                left=mid+1;
             }
-            else if (nums[mid] < target) {
-                left = mid + 1;
+            else if(nums[mid]< target){
+                left=mid+1;
             }
-            else {
-                right = mid - 1;
+            else{
+                right = mid-1;
             }
         }
-        return new int[]{first, last};
+        return new int[]{first,last};
     }
 }
